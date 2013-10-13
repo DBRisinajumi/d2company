a13516520a911a4296ed1881dd16e4417294c607
edmundspriede <edmunds@kls.lv>
2013-10-13 14:10:11 +0300

    export form

--- views/ccmpCompany/_export.php
+++ views/ccmpCompany/_export.php
@@ -15,14 +15,14 @@
             <?php echo CHtml::label(Yii::t('d2companyModule.crud', 'File name'),FALSE); ?>
             <?php echo CHtml::textField('file_name','report' ,array('size'=>100,'maxlength'=>200)); ?>
             <?php echo CHtml::label(Yii::t('d2companyModule.crud', 'Author'),FALSE); ?>
-            <?php echo CHtml::textField('author',Yii::app()->user->first_name ,array('size'=>100,'maxlength'=>200)); ?>
+            <?php echo CHtml::textField('author',Yii::app()->user->first_name." ".Yii::app()->user->last_name,array('size'=>100,'maxlength'=>200)); ?>
          
         </div>
 
     <div class="form-actions">
        
-                 <?php echo CHtml::submitButton('Excel',array('class'=>'excel_button')); ?>
-                 <?php echo CHtml::submitButton(Yii::t('d2companyModule.crud_static','PDF'),array('class'=>'excel_button')); ?>
+                 <?php echo CHtml::submitButton('',array('class' => 'excel-button')); ?>
+                 <?php echo CHtml::submitButton('', array('class' => 'pdf-button')); ?>
     </div>    
    
 
