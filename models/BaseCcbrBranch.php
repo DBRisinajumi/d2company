3ae63cec83de6ab9463e133c2652c039016a135e
edmundspriede <edmunds@kls.lv>
2013-11-09 13:51:48 +0200

    refactor

--- models/BaseCcbrBranch.php
+++ models/BaseCcbrBranch.php
@@ -7,7 +7,7 @@
  * @property string $ccbr_id
  * @property string $ccbr_ccmp_id
  * @property string $ccbr_name
- * @property string $ccrb_code
+ * @property string $ccbr_code
  * @property string $ccbr_notes
  * @property integer $ccbr_hide
  *
@@ -32,11 +32,11 @@ abstract class BaseCcbrBranch extends CActiveRecord
         return array_merge(
             parent::rules(), array(
                 array('ccbr_ccmp_id, ccbr_name', 'required'),
-                array('ccrb_code, ccbr_notes, ccbr_hide', 'default', 'setOnEmpty' => true, 'value' => null),
+                array('ccbr_code, ccbr_notes, ccbr_hide', 'default', 'setOnEmpty' => true, 'value' => null),
                 array('ccbr_hide', 'numerical', 'integerOnly' => true),
                 array('ccbr_ccmp_id', 'length', 'max' => 10),
                 array('ccbr_name', 'length', 'max' => 350),
-                array('ccrb_code', 'length', 'max' => 50),
+                array('ccbr_code', 'length', 'max' => 50),
                 array('ccbr_notes', 'safe'),
                 array('ccbr_id, ccbr_ccmp_id, ccbr_name, ccrb_code, ccbr_notes, ccbr_hide', 'safe', 'on' => 'search'),
             )
@@ -72,7 +72,7 @@ abstract class BaseCcbrBranch extends CActiveRecord
             'ccbr_id' => Yii::t('d2companyModule.crud', 'Fuel Station'),
             'ccbr_ccmp_id' => Yii::t('d2companyModule.crud', 'Ccbr Ccmp'),
             'ccbr_name' => Yii::t('d2companyModule.crud', 'Fuel Station'),
-            'ccrb_code' => Yii::t('d2companyModule.crud', 'Ccrb Code'),
+            'ccbr_code' => Yii::t('d2companyModule.crud', 'Code'),
             'ccbr_notes' => Yii::t('d2companyModule.crud', 'Ccbr Notes'),
             'ccbr_hide' => Yii::t('d2companyModule.crud', 'Ccbr Hide'),
         );
@@ -87,7 +87,7 @@ abstract class BaseCcbrBranch extends CActiveRecord
         $criteria->compare('t.ccbr_id', $this->ccbr_id, true);
         $criteria->compare('t.ccbr_ccmp_id', $this->ccbr_ccmp_id);
         $criteria->compare('t.ccbr_name', $this->ccbr_name, true);
-        $criteria->compare('t.ccrb_code', $this->ccrb_code, true);
+        $criteria->compare('t.ccbr_code', $this->ccbr_code, true);
         $criteria->compare('t.ccbr_notes', $this->ccbr_notes, true);
         $criteria->compare('t.ccbr_hide', $this->ccbr_hide);
 
