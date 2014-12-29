cc3637d4abb8bb7c327e247631badb21453cc040
Uldis Nelsons <uldisnelsons@gmail.com>
2014-12-29 09:26:12 +0200

    Remove unique from index ccmp_name

---
+++ migrations/m141229_072229_ccmp_name_unique_remove.php
@@ -0,0 +1,30 @@
+<?php
+
+class m141229_072229_ccmp_name_unique_remove extends EDbMigration
+{
+	public function up()
+	{
+        $this->execute("
+            ALTER TABLE `kls`.`ccmp_company`   
+                DROP INDEX `ccmp_name_UNIQUE`,
+                ADD  INDEX `ccmp_name_UNIQUE` (`ccmp_name`(4));
+        ");        
+	}
+
+	public function down()
+	{
+		echo "m141229_072229_ccmp_name_unique_remove does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
