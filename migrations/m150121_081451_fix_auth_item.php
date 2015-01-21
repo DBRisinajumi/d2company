7da41d7bbdf495358d62a065ecbbc1094718a8c1
Uldis Nelsons <uldisnelsons@gmail.com>
2015-01-21 10:47:54 +0200

    Added authitem fox migrate

---
+++ migrations/m150121_081451_fix_auth_item.php
@@ -0,0 +1,30 @@
+<?php
+
+class m150121_081451_fix_auth_item extends EDbMigration
+{
+	public function up()
+	{
+        $this->execute("
+            UPDATE `authitem` 
+            SET `description` = 'D2company.CcmpCompany module menu' 
+            WHERE `name` = 'D2company.CcmpCompany.Menu'; 
+        ");          
+	}
+
+	public function down()
+	{
+		echo "m150121_081451_fix_auth_item does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
